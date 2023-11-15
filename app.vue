e8e42efeeb3db874ca9ee6728f05253ddd9f9c16
hudengfei <hb.hudengfei@aisino.com>
2023-11-15 18:01:56 +0800

    init: 初始化nuxt3

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+ useSeoMeta({
+   title: 'hdfhdfhdfhdfhdfh',
+   description: 'hdftestnuxt3',
+   keywords: '1111111111111'
+ })
+</script>
+<template>
+  <NuxtLink to="/list">demo1</NuxtLink>
+  <NuxtLink to="/">首页</NuxtLink>
+  <NuxtPage />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // routeRules: {
+  //  '/list/demo1': { ssr: false }
+  // },
+  devtools: { enabled: true }
+})
